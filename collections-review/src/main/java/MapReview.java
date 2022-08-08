41a0d060f9e4b4f37ab5c824d2b966141bcee105
nurahmetemin <nurahmedemin@gmail.com>
2022-08-08 16:47:35 -0700

    Collections review - Map review added Map review class

---
+++ collections-review/src/main/java/MapReview.java
@@ -0,0 +1,14 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapReview {
+    public static void main(String[] args) {
+
+        Map<Integer,String> map = new HashMap<>();
+        map.put(1,"Sergiu");
+        map.put(2,"Moses");
+        System.out.println(map.get(2));
+
+
+    }
+}
