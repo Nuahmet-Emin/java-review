0d512470f4fadf65d485110fd14cdf779b034961
nurahmetemin <nurahmedemin@gmail.com>
2022-08-08 17:16:27 -0700

    Collections review - Map review complete

--- collections-review/src/main/java/MapReview.java
+++ collections-review/src/main/java/MapReview.java
@@ -9,6 +9,28 @@ public class MapReview {
         map.put(2,"Moses");
         System.out.println(map.get(2));
 
+      // find first non-repeating char in a string
+        System.out.println(firstNonRepeatingChar("deo java developer"));
+        
+    }
+    
+    public static Character firstNonRepeatingChar(String str){
+        Map<Character,Integer>   map = new HashMap<>();
+        int count;
+        char result;
+        for (Character ch : str.toCharArray()) {
+            if(map.containsKey(ch)){
+                count = map.get(ch);
+                map.put(ch, count+1);
+            }else {
+                map.put(ch, 1);
+            }
+        }
+        for(Character ch : str.toCharArray()){
+            if(map.get(ch) ==1 )
+                return ch;
+        }
+        return Character.MIN_VALUE;
 
     }
 }
