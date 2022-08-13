a20f16ff74416290c91e88cdb56f3db368c303db
nurahmetemin <nurahmedemin@gmail.com>
2022-08-13 08:57:09 -0700

    behaviour parameterization- lombok

--- lambda/src/main/java/ArraySorting.java
+++ lambda/src/main/java/ArraySorting.java
@@ -9,9 +9,25 @@ public class ArraySorting {
         as.sort(qs);
         as.sort(bs);
 
+     /*   MyLambda myLambda = () -> System.out.println("Sorting the array");
+        Sorting myLambda2 = () -> System.out.println("Sorting the array");*/
+        Sorting noLambda = new QuickSort();
+        noLambda.sort();
+
+        Sorting quickSort = () -> System.out.println("Quick Sorting");
+        as.sort(quickSort);
+
+        Sorting bubbleSort = () -> System.out.println("Bubble Sorting");
+        as.sort(bubbleSort);
+
     }
     private void sort(Sorting sorting){
         sorting.sort();
     }
 
 }
+
+
+/*interface MyLambda{
+    void print();
+}*/
