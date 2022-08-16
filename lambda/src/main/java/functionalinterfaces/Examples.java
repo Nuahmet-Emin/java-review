d71b6805956785a7077b9e418a5c7ea335fbb9af
nurahmetemin <nurahmedemin@gmail.com>
2022-08-15 19:20:56 -0700

    added functional interface-predicate example class and test

--- lambda/src/main/java/functionalinterfaces/Examples.java
+++ lambda/src/main/java/functionalinterfaces/Examples.java
@@ -1,4 +1,23 @@
 package functionalinterfaces;
 
+import java.util.function.Predicate;
+
 public class Examples {
+    public static void main(String[] args) {
+
+        //**********Predicate*********//
+        //anonymous class
+        /*Predicate<Integer>  lesserThan = new Predicate<Integer>() {
+            @Override
+            public boolean test(Integer integer) {
+                return integer<18;
+            }
+        };
+        System.out.println(lesserThan.test(40));*/
+
+
+        Predicate<Integer> lesserThan =  a ->  a<18; // implementation of test method that belongs to Predicate interface
+        System.out.println(lesserThan.test(8));
+
+    }
 }
