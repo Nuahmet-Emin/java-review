729feb13f1edd8fdb4ec826220740c7a63339784
nurahmetemin <nurahmedemin@gmail.com>
2022-08-16 22:18:29 -0700

    added double colon operator package and class for practice

--- lambda/src/main/java/task/OrangeTest.java
+++ lambda/src/main/java/task/OrangeTest.java
@@ -1,9 +1,14 @@
 package task;
 
+import java.sql.SQLOutput;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.function.Supplier;
 
-public class OrangeTest{
+public class OrangeTest {
     public static void main(String[] args) {
 
         List<Orange> inventory = new ArrayList<>();
@@ -13,9 +18,11 @@ public class OrangeTest{
         inventory.add(Orange.builder().weight(500).color(Color.RED).build());
         inventory.add(Orange.builder().weight(300).color(Color.GREEN).build());
 
+    }
+
 
         //behaviour
-        OrangeFormatter simpleFormatter =  orange -> "An orange of " + orange.getWeight() + "g";
+      /*  OrangeFormatter simpleFormatter =  orange -> "An orange of " + orange.getWeight() + "g";
         prettyPrintOrange(inventory,simpleFormatter);
 
         prettyPrintOrange(inventory,orange -> "An orange of " + orange.getWeight() + "g");
@@ -33,12 +40,25 @@ public class OrangeTest{
             String characteristic = orange.getWeight() > 150 ? "Heavy": "Light";
             return "A " + characteristic + " " + orange.getColor() + " orange";
         });
-    }
-
+    }*/
+/*
     private static void prettyPrintOrange(List<Orange> inventory, OrangeFormatter orangeFormatter){
         for (Orange orange : inventory) {
             String output = orangeFormatter.accept(orange);
             System.out.println(output);
         }
+    }*/
+
+
+
+
+    private static void prettyPrintOrange(List<Orange> inventory, Predicate<Orange> p) {
+
+        for (Orange orange : inventory) {
+            if(p.test(orange)  ){
+                System.out.println(orange.toString());
+            }
+        }
+
     }
 }
