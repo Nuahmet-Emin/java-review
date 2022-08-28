cb55fe69c9b1dea399117d1426fc27b45428a096
nurahmetemin <nurahmedemin@gmail.com>
2022-08-27 22:17:26 -0700

    Stream example-Intermediate operations

--- streams/src/main/java/StreamOperations.java
+++ streams/src/main/java/StreamOperations.java
@@ -5,7 +5,7 @@ public class StreamOperations {
     public static void main(String[] args) {
 
 
-        List<Integer> myList = Arrays.asList(1,2,3,4,3,5,7,85,9,1);
+        List<Integer> myList = Arrays.asList(1,2,3,4,3,5,7,85,9,1,44);
         myList.forEach(System.out::println);
 
         //FILTER
@@ -14,6 +14,27 @@ public class StreamOperations {
                 .filter(i -> i %3 == 0)
                 .distinct()
                 .forEach(System.out::println);
+
+        //LIMIT
+        System.out.println("LIMIT");
+        myList.stream()
+                .filter(i-> i %2 == 0)
+                .limit(1)
+                .forEach(System.out::println);
+        //SKIP
+        System.out.println("SKIP");
+        myList.stream()
+                .filter(i-> i % 2 == 0)
+                .skip(2)
+                .forEach(System.out::println);
+
+        //MAP
+        System.out.println("MAP");
+        myList.stream()
+                .map(x-> x *2)
+                .filter(i-> i %3 == 0)
+                .forEach(System.out::println);
+
 
     }
 }
