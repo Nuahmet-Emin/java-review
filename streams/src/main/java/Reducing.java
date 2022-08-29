9f46d88e0088fda6d19dc38779d4e88500544e30
nurahmetemin <nurahmedemin@gmail.com>
2022-08-28 18:24:13 -0700

    Stream-Reduce

---
+++ streams/src/main/java/Reducing.java
@@ -0,0 +1,33 @@
+import javax.swing.text.html.Option;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+public class Reducing {
+    public static void main(String[] args) {
+
+        List<Integer> numbers = Arrays.asList(4,5,3,9);
+      int result =   numbers.stream().reduce(1,(a,b)-> a+b);
+        System.out.println(result);
+
+        //No Initial Value
+      Optional<Integer>  result2 =numbers.stream().reduce((a, b)-> a+b);
+        System.out.println(result2.get());
+
+        //Max and Min
+      Optional<Integer> min =  numbers.stream().reduce(Integer::min);
+        Optional<Integer> max = numbers.stream().reduce(Integer::max);
+        Optional<Integer> sum = numbers.stream().reduce(Integer::sum);
+
+        System.out.println("Min:" + min.get());
+        System.out.println("Max:" + max.get());
+        System.out.println("Sum:" + sum.get());
+
+
+
+
+
+
+
+    }
+}
