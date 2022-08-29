b8ccae050c842bdb595bba7f14c91e35a5973dad
nurahmetemin <nurahmedemin@gmail.com>
2022-08-28 17:08:16 -0700

    Flatmap example- employee task

---
+++ streams/src/main/java/EmployeeTest.java
@@ -0,0 +1,34 @@
+import java.util.List;
+
+public class EmployeeTest {
+
+
+    public static void main(String[] args) {
+        //Print all emails - One employee has one email - One to One
+        EmployeeData.readAll()
+                .map(Employee:: getEmpEmail)
+                .forEach(System.out::println);
+        System.out.println("***********************");
+
+        //Print all phone numbers
+        EmployeeData.readAll()
+                .map(Employee::getEmpPhoneNumbers)
+                .forEach(System.out::println);
+        System.out.println("***********************");
+
+        //Flatmap
+        EmployeeData.readAll()
+                .flatMap(employee -> employee.getEmpPhoneNumbers().stream())
+                .forEach(System.out::println);
+
+        System.out.println("***********************");
+        EmployeeData.readAll()
+                .map(Employee::getEmpPhoneNumbers)
+                .flatMap(List::stream)
+                .forEach(System.out::println);
+
+
+
+
+    }
+}
