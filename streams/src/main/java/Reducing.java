984db51eb0ba8a73eb3178f7b6666fe645dba551
nurahmetemin <nurahmedemin@gmail.com>
2022-08-28 18:39:49 -0700

    Stream-Reduce- map and count task

--- streams/src/main/java/Reducing.java
+++ streams/src/main/java/Reducing.java
@@ -24,9 +24,12 @@ public class Reducing {
         System.out.println("Sum:" + sum.get());
 
 
+        //Task: count the number of dish from the dish data
+       int dishCount = DishData.getAll().stream().map(dish -> 1 ).reduce(0,(a,b)-> a+b);
+        System.out.println(dishCount);
 
-
-
+       long dCount = DishData.getAll().stream().count();
+        System.out.println(dCount);
 
 
     }
